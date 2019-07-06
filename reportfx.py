46efd37eb8b6066407a8a1de97e260a216febd23
Eric Snover <ersnover@gmail.com>
2019-07-06 16:30:35 -0500

    rough outline of report functions. more coming soon

---
+++ reportfx.py
@@ -0,0 +1,11 @@
+import json
+import time
+
+def Create_Report():
+    pass        #will create printable text file containing archived game data
+
+def Print_Report():
+    pass        #prints report [called from Create_Report()]
+
+def Email_Report():
+    pass        #emails report [called from Create_Report()]
